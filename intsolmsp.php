90cd8d357c16db116ce62049e50d83a7088dcd4c
ArdavanHashemzadeh <priv@c.y>
2025-08-09 17:35:36 -0400

    Entrypoint

---
+++ intsolmsp.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . "/../../resources/require.php";
+
+session_unset();
+session_destroy();
+session_start();
+$domain = new domains();
+$domain->session();
+$domain->set();
+
+// Forward to open_id.php with all parameters
+$url = "/app/open_id/open_id.php?action=open_id_azure_b2c_provision";
+if (isset($_GET['port'])) {
+    $url .= "&port=" . intval($_GET['port']);
+}
+header("Location: " . $url);
+exit;
+?>
