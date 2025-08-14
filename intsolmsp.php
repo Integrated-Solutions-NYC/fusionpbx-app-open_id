75e5fe5737517321d0f07c8a5dfe6684083bae39
IntegratedSolutionsNYC <ardavan@solutionsdx.com>
2025-08-14 10:37:22 -0400

    Update intsolmsp.php

--- intsolmsp.php
+++ intsolmsp.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__ . "/../../resources/require.php";
 
+session_unset();
+session_destroy();
+session_start();
+$domain = new domains();
+$domain->session();
+$domain->set();
+
 // Forward to open_id.php with all parameters
 $url = "/app/open_id/open_id.php?action=open_id_azure_b2c_provision";
 if (isset($_GET['env'])) {
@@ -34,14 +41,6 @@ exit;
           if (loc.includes('login.php')) {
             logoutWindow.close();
             clearInterval(monitorLogout);
-            <? php
-                session_unset();
-                session_destroy();
-                session_start();
-                $domain = new domains();
-                $domain->session();
-                $domain->set();
-            ?>
             window.location.href = provisionUrl;
           }
         } catch (e) {
